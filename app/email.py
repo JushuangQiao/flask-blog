82617ae9cd9238286ad2ce7a2dec33c85704c5e1
qiaojushuang <747848783@qq.com>
2017-08-07 10:35:50 +0800

    修改格式

--- app/email.py
+++ app/email.py
@@ -14,7 +14,8 @@ def send_async_email(app, msg):
 
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
-    msg = Message(Config.FLASKY_MAIL_SUBJECT_PREFIX+subject, sender=Config.FLASKY_MAIL_SENDER, recipients=[to])
+    msg = Message(Config.FLASKY_MAIL_SUBJECT_PREFIX+subject,
+                  sender=Config.FLASKY_MAIL_SENDER, recipients=[to])
     # 邮件内容会以文本和html两种格式呈现，而你能看到哪种格式取决于你的邮件客户端
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
